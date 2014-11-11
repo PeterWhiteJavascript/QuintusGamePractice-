5bd3e409a31217aa1e5c4a290d89c5d3ce97ec81
PeterWhiteJavascript <pedroblanco3257@gmail.com>
2014-11-11 12:17:33 -0800

    Update game.js

--- game.js
+++ game.js
@@ -2,18 +2,20 @@ window.addEventListener("load",function() {
   var Q = window.Q = Quintus()
     .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
     .setup({
-      width: 320;
-      height:180;
+      width: 720,
+      height:480,
       scaleToFit: true
-  }).controles().touch();
+  }).controls().touch();
   Q.setImageSmoothing(false);
   
   //define sene
   Q.scene("level", function(stage){
     Q.stageTMX("su.tmx", stage);
+    
   });
   //load assets
   Q.loadTMX("su.tmx", function(){
-    Q.stageScene("level");
+    Q.stageScene("level",0);
   });
+  
 });
